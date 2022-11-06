81dc32a71a4d2e6dc82aa60639258718e2a3d764
CHEOLHWAN BAE <70792311+armyants531@users.noreply.github.com>
2022-11-07 00:06:04 +0900

    [Platinum V] Title: Counting Inversions, Time: 192 ms, Memory: 9836 KB -BaekjoonHub

--- 백준/Platinum/10090. Counting Inversions/Counting Inversions.cc
+++ 백준/Platinum/10090. Counting Inversions/Counting Inversions.cc
@@ -1,39 +1,51 @@
-// Counting Inversions // 세그먼트 트리
+// Counting Inversions // merge sort
+// https://eine.tistory.com/entry/%EB%B0%B1%EC%A4%80-10090%EB%B2%88-%EB%AC%B8%EC%A0%9C-Counting-Inversions-%EB%AC%B8%EC%A0%9C-%ED%92%80%EC%9D%B4
+// https://www.geeksforgeeks.org/inversion-count-in-array-using-merge-sort/ 참고
 #include <iostream>
-#include <vector>
-#include <algorithm> // lower_bound, unique, sort
 
 using namespace std;
+typedef long long ll;
 
-long long tree[1000000 * 4] = { 0, }; // 세그먼트 트리 초기화
+int *arr, *tmp;
+ll inv_cnt = 0; // inverse 쌍의 개수
 
-// [left, right] 구간 합을 구하는 함수
-long long interval_sum(int start, int end, int index, int left, int right) {
-	if (start > right || end < left) // 구간이 겹치지 않는 경우
-		return 0;
-	else if (left <= start && end <= right)
-		return tree[index];
-	else {
-		int mid = (start + end) / 2;
-		return interval_sum(start, mid, index * 2, left, right) + interval_sum(mid + 1, end, index * 2 + 1, left, right);
+void merge(int start, int mid, int end) {
+	int i = start; // 왼쪽 배열의 인덱스
+	int j = mid; // 오른쪽 배열의 인덱스
+	int k = start; // 합친 배열(tmp)의 인덱스
+	while ((i <= mid - 1) && (j <= end - 1)) {
+		if (arr[i] > arr[j]) {
+			tmp[k] = arr[j];
+			k++; j++;
+			inv_cnt += mid - i; // 왼쪽 배열에 남아있는 원소의 수(mid - i) = inverse 쌍의 수
+		}
+		else { // arr[i] <= arr[j]	
+			tmp[k] = arr[i];
+			k++; i++;
+		}
 	}
+	// 남은 왼쪽 배열의 원소를 tmp에 복사
+	while (i <= mid - 1) {
+		tmp[k] = arr[i];
+		k++; i++;
+	}
+	// 남은 오른쪽 배열의 원소를 tmp에 복사
+	while (j <= end - 1) {
+		tmp[k] = arr[j];
+		k++; j++;
+	}
+	// 원본 배열에 tmp배열 원소 복사
+	for (i = start; i <= end - 1; i++) {
+		arr[i] = tmp[i];
+	} 
 }
 
-// 특정 인덱스의 값에 1을 더해 주는 함수
-// 배열에 어떤 수(현재 코드에서는 change_index - 1)가 추가되었는지 표시한다
-void update(int start, int end, int index, int change_index) {
-	if (change_index < start || change_index > end) // 범위 밖이면
-		return;
-	else if (start == end) { // left node이면
-		tree[index]++;
-		return;
-	}
-	else {
-		tree[index]++;
-		int mid = (start + end) / 2;
-		// 자식 노드 정보 업데이트
-		update(start, mid, index * 2, change_index);
-		update(mid + 1, end, index * 2 + 1, change_index);
+void mergeSort(int start, int end) { // [start, end)
+	int mid = (start + end) / 2;
+	if (start != mid) { // start == mid이면 실행하지 않음
+		mergeSort(start, mid); // [start, mid)
+		mergeSort(mid, end); // [mid, end)
+		merge(start, mid, end);
 	}
 }
 
@@ -44,24 +56,15 @@ int main() {
 
 	int n;
 	cin >> n;
-	vector<int> v; // 원본 벡터(sort 시행)
-	vector<int> copy; // 원래 벡터의 복사본 
+	arr = new int[n]();
+	tmp = new int[n]();
 	for (int i = 0; i < n; i++) {
-		int val;
-		cin >> val;
-		v.push_back(val);
-		copy.push_back(val);
+		cin >> arr[i];
 	}
-	long long count = 0;
-	sort(v.begin(), v.end()); // 정렬
-	// 처음에는 update만 한다.
-	update(0, n - 1, 1, copy[0]);
-	for (int i = 1; i < n; i++) {
-		int coord = copy[i] - 1;
-		count += interval_sum(0, n - 1, 1, coord + 1, n - 1); // [coord + 1, n - 1] 구간 합
-		update(0, n - 1, 1, coord);
-	}
+	mergeSort(0, n); // [0, n)
+	cout << inv_cnt << "\n";
 
-	cout << count << "\n";
+	delete[] arr;
+	delete[] tmp;
 	return 0;
 }
