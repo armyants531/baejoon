e3afc4d09bb4effb505740b8724d9508d1ad7d22
CHEOLHWAN BAE <70792311+armyants531@users.noreply.github.com>
2022-11-13 23:06:58 +0900

    [Silver IV] Title: 설탕 배달, Time: 0 ms, Memory: 2040 KB -BaekjoonHub

---
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.cc
@@ -0,0 +1,29 @@
+// 설탕 배달  // dp(tabulation)  // math
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int table[5001] = { 0, }; // bottom-up
+
+int main() {
+	int N;
+	cin >> N;
+	table[3] = 1;
+	table[5] = 1;
+
+	for (int i = 6; i <= N; i++) {
+		if (table[i - 5] && table[i - 3]) {
+			table[i] = min(table[i - 5], table[i - 3]) + 1;
+		}
+		else if (table[i - 5]) {
+			table[i] = table[i - 5] + 1;
+		}
+		else if (table[i - 3]) {
+			table[i] = table[i - 3] + 1;
+		}
+	}
+	if (table[N] != 0)
+		cout << table[N] << "\n";
+	else
+		cout << -1 << "\n";
+}
