572c349e90a7e2d7db26fe458ad55ea048b8133b
CHEOLHWAN BAE <70792311+armyants531@users.noreply.github.com>
2022-11-23 23:25:41 +0900

    [Platinum V] Title: LCS 4, Time: 24 ms, Memory: 2804 KB -BaekjoonHub

--- 백준/Platinum/13711. LCS 4/LCS 4.cc
+++ 백준/Platinum/13711. LCS 4/LCS 4.cc
@@ -12,7 +12,6 @@ int main() {
 	cin >> N;
 	vector<int> A(100000);
 	vector<int> B(100000);
-	vector<int> AB(100000);
 	int p;
 	for (int i = 0; i < N; i++) {
 		cin >> p; 
@@ -20,16 +19,14 @@ int main() {
 	}
 	for (int i = 0; i < N; i++) {
 		cin >> p;
-		B.push_back(p - 1);
-		AB[i] = A[p - 1]; // A[B[i]]
+		B[i] = A[p - 1];
 	}
-
-	// AB[i]의 LIS 구하기 
+	// B[i]의 LIS 구하기 
 	vector<int> vec;
 	int len = 0;
 	vec.push_back(-1); // 처음에 입력 가능한 수열의 원소보다 작은 수를 삽입(처음 원소를 항상 삽입하기 위해)
 	for (int i = 0; i < N; i++) {
-		int val = AB[i];
+		int val = B[i];
 		if (vec.back() >= val) {
 			// lower_bound: 찾으려는 val(key) 값보다 같거나 큰 숫자가 배열 몇 번째에서 처음 등장하는지를 리턴(반환형: iterator)
 			int lb = lower_bound(vec.begin(), vec.end(), val) - vec.begin();  // 인덱스 구하기
