3e73f627aabc64bb0c3b0d25b23f781dea2682d5
CHEOLHWAN BAE <70792311+armyants531@users.noreply.github.com>
2022-12-18 13:22:17 +0900

    [Bronze III] Title: 택시 기하학, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3053. 택시 기하학/택시 기하학.cc
@@ -0,0 +1,15 @@
+// 택시 기하학  // Geometry
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	double R;
+	const double pi = 3.14159265358979;
+	cin >> R;
+	cout << fixed;
+	cout.precision(6);
+	cout << pi * R * R << "\n";
+	cout << R * R * 2 << "\n";
+	return 0;
+}
