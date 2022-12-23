a0abb93414ce36280e103587f5932e93938ba5a7
CHEOLHWAN BAE <70792311+armyants531@users.noreply.github.com>
2022-12-23 18:59:23 +0900

    [Bronze V] Title: 나머지, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int A, B, C;
+	cin >> A >> B >> C;
+	cout << (A + B) % C << "\n";
+	cout << ((A % C) + (B % C)) % C << "\n";
+	cout << (A * B) % C << "\n";
+	cout << ((A % C) * (B % C)) % C << "\n";
+	
+	return 0;
+}
