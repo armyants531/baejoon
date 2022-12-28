7d51d3207fe5e33d2bca6ca79b9fbc724151d50b
CHEOLHWAN BAE <70792311+armyants531@users.noreply.github.com>
2022-12-28 20:11:40 +0900

    [Gold I] Title: KUBC League (Small), Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/14590. KUBC League （Small）/KUBC League （Small）.cc
+++ 백준/Gold/14590. KUBC League （Small）/KUBC League （Small）.cc
@@ -1,59 +1,46 @@
-// KUBC League (Small)  // DP  // bitmasking  // TSP(Travelling Salesman Problem)
+// KUBC League (Small)  // dfs
+// 인접행렬 만든 후, dfs를 시행하면서 뒤에서부터 노드의 값을 stack에 저장 
 #include <bits/stdc++.h>
 
 using namespace std;
-int cache[20][1 << 20];  // save max len
-vector<vector<int>> arr(20, vector<int>(20));
 
-int memo(int current, int status, int N) {
-    // 방문 처리
-    status |= (1 << current);
-    int& ans = cache[current][status];
-    if (status == (1 << N) - 1) { // if all visited
-        //cout << status << "\n";
-        return ans = 1;
-    }
-    if (ans != -1) { // already calc
-        return ans;
-    }
-    ans = 1;
-    for (int next = 0; next < N; next++) {
-        if (arr[current][next] != 0 && (status & (1 << next)) == 0) { // if can visit && not visited
-            ans = max(ans, memo(next, status, N) + 1);
-        }
-    }
-    //cout << status << "\n";
-    return ans;
-}
+vector<vector<int>> adj;
+vector<bool> visited;
+stack<int> st;
 
-void tracking(int current, int status, int N) {
-    for (int next = 0; next < N; next++) {
-        if (arr[current][next] != 0 && (status & (1 << next)) == 0) { // if can visit && not visited
-            if (cache[current][status] == cache[next][status | (1 << next)] + 1) {
-                cout << next + 1 << " ";
-                tracking(next, status | (1 << next), N);
-                break;
-            }
-        }
-    }
+int dfs(int curr, int cnt) {
+	visited[curr] = true;
+	for (auto next : adj[curr]) {
+		if (!visited[next]) {
+			cnt = max(cnt, dfs(next, cnt + 1)); 
+		}
+	}
+	st.push(curr);
+	return cnt; 
 }
 
 int main() {
-    ios_base::sync_with_stdio(false);
+	ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
     int N;
     cin >> N;
-    memset(cache, -1, sizeof(cache));
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            cin >> arr[i][j];
-        }
-    }
-    cout << memo(0, 0, N) << "\n";
-    cout << 1 << " ";
-    tracking(0, 1, N); // status: 1에서 시작
-    cout << "\n";
-
-    return 0;
+    adj.resize(N + 1);
+    visited.resize(N + 1, false);
+    for (int i = 1; i <= N; i++) {
+    	for (int j = 1; j <= N; j++) {
+    		int val;
+    		cin >> val;
+    		if (val == 1) {
+    			adj[i].push_back(j); 
+			}
+		}
+	}
+	cout << dfs(1, 1) << "\n";
+	while (!st.empty()) {
+		cout << st.top() << " ";
+		st.pop();
+	}
+	cout << "\n";
+	return 0;
 }
