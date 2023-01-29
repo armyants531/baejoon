2aedb0929d161a7431db780f55733a3a7c37f3c7
CHEOLHWAN BAE <70792311+armyants531@users.noreply.github.com>
2023-01-29 18:43:35 +0900

    [Bronze V] Title: 그대로 출력하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	string s;
+	getline(cin, s);
+	while (s != "") {
+		cout << s << "\n";
+		getline(cin, s);
+	}
+	
+	return 0;
+}
