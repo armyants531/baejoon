f10e5b94e442ba2bd829e6bca660bf3229bf615a
CHEOLHWAN BAE <70792311+armyants531@users.noreply.github.com>
2023-03-01 22:56:56 +0900

    [Bronze III] Title: 직사각형에서 탈출, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1085. 직사각형에서 탈출/직사각형에서 탈출.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int x, y, w, h;
+	cin >> x >> y >> w >> h;
+	cout << min({x, y, w - x, h - y}) << "\n";
+	
+	return 0;
+}
