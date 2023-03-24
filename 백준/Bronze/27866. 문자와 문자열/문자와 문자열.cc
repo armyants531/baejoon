1ecd0b505ac27d516a36b0af493e773b0fdeb22b
CHEOLHWAN BAE <70792311+armyants531@users.noreply.github.com>
2023-03-25 00:52:00 +0900

    [Bronze V] Title: 문자와 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	string s;
+	int i;
+	cin >> s >> i;
+	cout << s[i - 1] << "\n";
+	
+	return 0;
+}
