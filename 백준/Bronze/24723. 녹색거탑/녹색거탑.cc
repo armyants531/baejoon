71a076417b9e24cd5d23ceb2dadc5730a1848b0a
CHEOLHWAN BAE <70792311+armyants531@users.noreply.github.com>
2023-06-02 16:46:42 +0900

    [Bronze IV] Title: 녹색거탑, Time: 0 ms, Memory: 2208 KB -BaekjoonHub

---
+++ 백준/Bronze/24723. 녹색거탑/녹색거탑.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int N;
+	cin >> N;
+	cout << pow(2, N) << "\n";
+}
