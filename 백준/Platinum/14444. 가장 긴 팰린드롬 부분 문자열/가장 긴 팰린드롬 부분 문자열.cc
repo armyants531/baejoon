dff823dfd2d53f54f925a6902e4463ded2a68e7a
CHEOLHWAN BAE <70792311+armyants531@users.noreply.github.com>
2023-06-12 22:21:44 +0900

    [Platinum V] Title: 가장 긴 팰린드롬 부분 문자열, Time: 4 ms, Memory: 3500 KB -BaekjoonHub

--- 백준/Platinum/14444. 가장 긴 팰린드롬 부분 문자열/가장 긴 팰린드롬 부분 문자열.cc
+++ 백준/Platinum/14444. 가장 긴 팰린드롬 부분 문자열/가장 긴 팰린드롬 부분 문자열.cc
@@ -1,4 +1,4 @@
-// 가장 긴 팰린드롬 부분 문자열  // dp
+// 가장 긴 팰린드롬 부분 문자열  // dp  // Manacher's Algorithm
 // https://www.crocus.co.kr/1075 참고
 // 10000이상의 길이를 가진 문자열의 부분 문자열 -> O(n)에 구하기
 // A[i] : i번째 문자를 중심으로 하는 가장 긴 회문의 반지름 크기
@@ -12,16 +12,16 @@ void manacher(string s, int n) {
 	// r: 각 인덱스에 대해 회문이 될 수 있는 최대 범위  
 	// p: r이 형성되는 중심점
 	int r = 0; // r = max(j + A[j]) (for any j < i)
-	int p = 0; // p = j_max
+	int p = 0; // p = j_max, r = p + A[p]
 	for (int i = 0; i < n; i++) {
 		// A[i]의 초기값 설정
 		if (i > r) {
 			A[i] = 0;
 		}
 		else { // i <= r
-			A[i] = min(r - i, A[2 * p - i]);
+			A[i] = min(r - i, A[2 * p - i]); // 2 * p - i: p에 대해 i와 대칭인 점
 		}
-		// s[i - A[i]] != s[i + A[i]] 가 되기 전까지 A[i] 값 증가
+		// s[i - A[i] - 1] != s[i + A[i] + 1]가 되기 전까지 A[i] 값 증가
 		while (i - A[i] >= 1 && i + A[i] < n - 1 && s[i - A[i] - 1] == s[i + A[i] + 1]) {
 			A[i]++;
 		}
@@ -33,7 +33,7 @@ void manacher(string s, int n) {
 }
 
 int main() {
-	ios_base::sync_with_stdio;
+	ios_base::sync_with_stdio(NULL);
 	cin.tie(NULL);
 	cout.tie(NULL);
 	string s;
