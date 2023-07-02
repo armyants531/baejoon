0da0fd62d3934dfad9c1c39cca2ae1794015bd1b
CHEOLHWAN BAE <70792311+armyants531@users.noreply.github.com>
2023-07-02 16:13:20 +0900

    [Silver II] Title: 더 흔한 타일 색칠 문제, Time: 24 ms, Memory: 2288 KB -BaekjoonHub

--- 백준/Silver/28298. 더 흔한 타일 색칠 문제/더 흔한 타일 색칠 문제.cc
+++ 백준/Silver/28298. 더 흔한 타일 색칠 문제/더 흔한 타일 색칠 문제.cc
@@ -17,50 +17,41 @@ int main() {
 			A[i][j] = s[j];
 		}
 	}
-	vector<vector<vector<int>>> B(K, vector<vector<int>>(K, vector<int>(26)));
-	for (int i = 0; i < N; i += K) {
-		for (int j = 0; j < M; j += K) {
-			for (int di = 0; di < K; di++) {
-				for (int dj = 0; dj < K; dj++) {
-					B[di][dj][A[i + di][j + dj] - 'A']++;
-				}
-			}
-		}
-	}
 	int sum = 0;
-	vector<vector<char>> C(K, vector<char>(K));
-	int repeat = N * M / (K * K);
 	for (int i = 0; i < K; i++) {
 		for (int j = 0; j < K; j++) {
-			int max_idx = -1;
-			int max_v = 0;
-			for (int k = 0; k < 26; k++) {
-				if (B[i][j][k] > max_v) {
-					max_idx = k;
-					max_v = B[i][j][k];
+			vector<int> alp(26);
+			for (int di = i; di < N; di += K) {
+				for (int dj = j; dj < M; dj += K) {
+					alp[A[di][dj] - 'A']++;
 				}
 			}
-			sum += repeat - max_v;
-			C[i][j] = (char)(max_idx + 'A');
+			int max_v = 0;
+			int max_idx = -1;
+			for (int k = 0; k < 26; k++) {
+				if (max_v < alp[k]) {
+					max_v = alp[k];
+					max_idx = k;
+				}
+			}
+			char c = 'A' + max_idx;
+			for (int di = i; di < N; di += K) {
+				for (int dj = j; dj < M; dj += K) {
+					if (A[di][dj] != c) {
+						A[di][dj] = c;
+						sum++;
+					}
+				}
+			}
 		}
 	}
 	cout << sum << "\n";
-	vector<vector<char>> ans(N, vector<char>(M));
-	for (int i = 0; i < N; i += K) {
-		for (int j = 0; j < M; j += K) {
-			for (int di = 0; di < K; di++) {
-				for (int dj = 0; dj < K; dj++) {
-					ans[i + di][j + dj] = C[di][dj];
-				}
-			}
-		}
-	}
 	for (int i = 0; i < N; i++) {
 		for (int j = 0; j < M; j++) {
-			cout << ans[i][j];
+			cout << A[i][j];
 		}
 		cout << "\n";
 	}
-
-	return 0;
+	
+	return 0;	
 }
