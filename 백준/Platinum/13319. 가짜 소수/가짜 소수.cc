dbc74a9665b205542c5d9dd7aa0fdfb5361dee48
CHEOLHWAN BAE <70792311+armyants531@users.noreply.github.com>
2023-07-14 15:56:28 +0900

    [Platinum V] Title: 가짜 소수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Platinum/13319. 가짜 소수/가짜 소수.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#include <random>
+#define int long long
+
+using namespace std;
+typedef long long ll;
+
+signed main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	cout << 464052305161 << " " << 4261;
+
+	return 0;
+}
