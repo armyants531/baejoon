287dfb822a10634aa6e4f6a79b7ecd318d9c6439
CHEOLHWAN BAE <70792311+armyants531@users.noreply.github.com>
2023-07-22 22:53:05 +0900

    [Platinum III] Title: NMK, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Platinum/1201. NMK/NMK.cc
+++ 백준/Platinum/1201. NMK/NMK.cc
@@ -1,59 +1,46 @@
-// NMK  // greedy
-// https://hongjw1938.tistory.com/191 참고
-// M + K - 1 <= N <= M x K
 #include <bits/stdc++.h>
-
+#define int long long
+ 
 using namespace std;
-typedef long long ll;
-
-int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-    int N, M, K;
-    cin >> N >> M >> K;
-    queue<int> que; // 1~N 까지의 수 저장
-    for (int i = 1; i <= N; i++) {
-        que.push(i);
-    }
-    if (M + K - 1 <= N && N <= M * K) {
-        vector<stack<int>> st(M); // M개의 감소 수열 저장
-        // 길이가 K인 감소 수열 만들기(1~K)
-        for (int i = 0; i < K; i++) {
-            st[0].push(que.front());
-            que.pop();
-        }
-        if (M != 1) {
-            // N - K 개를 M - 1개의 감소 수열로 만들기
-            int q = (N - K) / (M - 1);
-            int r = (N - K) % (M - 1);
-            // q + 1 길이의 감소 수열 r개 생성
-            for (int i = 1; i <= r; i++) {
-                for (int j = 1; j <= q + 1; j++) {
-                    st[i].push(que.front());
-                    que.pop();
-                }
-            }
-            // q 길이의 감소 수열 M - 1 - r개 생성
-            for (int i = r + 1; i <= M - 1; i++) {
-                for (int j = 1; j <= q; j++) {
-                    st[i].push(que.front());
-                    que.pop();
-                }
-            }
-        }
-        // 출력
-        for (int i = 0; i < M; i++) {
-            while (!st[i].empty()) {
-                cout << st[i].top() << " ";
-                st[i].pop();
-            }
-        }
-        cout << "\n";
-    }
-    else {
-        cout << -1 << "\n";
-    }
-
-    return 0;
+ 
+signed main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int N, M, K;
+	cin >> N >> M >> K;
+	if (N < M + K - 1 || N > M * K) {
+		cout << -1 << "\n";
+	}
+	else {
+		if (K == 1) {
+			for (int i = 1; i <= N; i++) {
+				cout << i << " ";
+			}
+			cout << "\n";
+		}
+		else {
+			//vector<vector<int>> arr(K);
+			int q = (N - M) / (K - 1);
+			int r = (N - M) % (K - 1);
+			// size M: 1
+			for (int i = N - M + 1; i <= N; i++) {
+				cout << i << " ";
+			}
+			// size q+1: r
+			for (int i = 1; i <= r; i++) {
+				for (int j = N - M + 1 - i * (q + 1); j <= N - M - (i - 1) * (q + 1); j++) {
+					cout << j << " ";
+				}
+			}
+			// size q: K-1-r
+			for (int i = 1; i <= K-1-r; i++) {
+				for (int j = N - M + 1 - r * (q + 1) - i * q; j <= N - M - r * (q + 1) - (i - 1) * q; j++) {
+					cout << j << " ";
+				}
+			}	
+		}
+	}
+	
+	return 0;
 }
