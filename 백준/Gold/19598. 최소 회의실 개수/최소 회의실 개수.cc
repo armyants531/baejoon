9062cbd6dc7dc44befe8108eb0228877446acf74
CHEOLHWAN BAE <70792311+armyants531@users.noreply.github.com>
2023-08-01 12:46:04 +0900

    [Gold V] Title: 최소 회의실 개수, Time: 48 ms, Memory: 5224 KB -BaekjoonHub

--- 백준/Gold/19598. 최소 회의실 개수/최소 회의실 개수.cc
+++ 백준/Gold/19598. 최소 회의실 개수/최소 회의실 개수.cc
@@ -1,4 +1,4 @@
-// 최소 회의실 개수 
+// 최소 회의실 개수
 #include <bits/stdc++.h>
 #define int long long
  
@@ -13,24 +13,24 @@ signed main() {
 	cin >> N;
 	vector<pii> arr;
 	int x, y;
-	set<int> s;
 	for (int i = 0; i < N; i++) {
 		cin >> x >> y;
-		s.insert(x);
-		s.insert(y);
-		arr.push_back({x, 1});
-		arr.push_back({y, -1});
+		arr.push_back({x, y});
 	}
-	sort(arr.begin(), arr.end());
+	sort(arr.begin(), arr.end()); // 시작 시간 오름차순 
 	int ans = 0;
-	int idx = 0;
-	int pre = 0;
-	for (auto t : s) {
-		while (idx < arr.size() && t >= arr[idx].first) {
-			pre += arr[idx].second;
-			idx++;
+	priority_queue<int, vector<int>, greater<int>> pq; // min_heap, 끝나는 시간 저장 
+	for (int i = 0; i < N; i++) {
+		if (pq.empty()) {
+			pq.push(arr[i].second);
+		} 
+		else {
+			while (!pq.empty() && pq.top() <= arr[i].first) { // 이전 회의 끝나는 시간 <= 현 회의 시작 시간  
+				pq.pop();
+			}
+			pq.push(arr[i].second);
 		}
-		ans = max(ans, pre);
+		ans = max(ans, (int)pq.size());
 	}
 	
 	cout << ans << "\n";
