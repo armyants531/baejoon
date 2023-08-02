d964ef1d5fee75e28ca3b704bc3e49dcfa5a3625
CHEOLHWAN BAE <70792311+armyants531@users.noreply.github.com>
2023-08-03 00:48:22 +0900

    [Gold V] Title: 더하기와 나누기, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/28426. 더하기와 나누기/더하기와 나누기.cc
@@ -0,0 +1,20 @@
+// 더하기와 나누기
+#include <bits/stdc++.h>
+#define int long long
+ 
+using namespace std;
+using pii = pair<int, int>;
+ 
+signed main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int N;
+	cin >> N;
+	cout << 3 << " ";
+	for (int i = 1; i < N; i++) {
+		cout << 6 * i << " ";
+	}
+	
+	return 0;
+}
