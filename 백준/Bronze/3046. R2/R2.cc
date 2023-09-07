5e2502cb53af2f326dab809d3eb6664abbd285ba
CHEOLHWAN BAE <70792311+armyants531@users.noreply.github.com>
2023-09-07 23:05:56 +0900

    [Bronze IV] Title: R2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3046. R2/R2.cc
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#define int long long
+#define fi first
+#define se second
+
+using namespace std;
+using pii = pair<int, int>;
+
+signed main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int R1, S;
+	cin >> R1 >> S;
+	cout << 2 * S - R1 << "\n";
+	
+	return 0;
+}
