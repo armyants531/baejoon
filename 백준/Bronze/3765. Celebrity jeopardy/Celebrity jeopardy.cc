09094201f71135444642c8c863b5c5273047ee21
CHEOLHWAN BAE <70792311+armyants531@users.noreply.github.com>
2023-09-29 23:18:07 +0900

    [Bronze IV] Title: Celebrity jeopardy, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/3765. Celebrity jeopardy/Celebrity jeopardy.cc
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#define int long long
+#define fi first
+#define se second
+
+using namespace std;
+using pii = pair<int, int>;
+
+signed main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	string a;
+	while (true) {
+		getline(cin, a);
+		if (cin.eof()) {
+			break;
+		}
+		cout << a << "\n";
+	}
+	
+	return 0;
+}
