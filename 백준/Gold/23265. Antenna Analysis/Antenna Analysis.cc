3da74e99bf5d33bcb94d6bfe3d7af147b05e3ac0
CHEOLHWAN BAE <70792311+armyants531@users.noreply.github.com>
2023-11-19 15:47:37 +0900

    [Gold III] Title: Antenna Analysis, Time: 100 ms, Memory: 14532 KB -BaekjoonHub

---
+++ 백준/Gold/23265. Antenna Analysis/Antenna Analysis.cc
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+#define int long long
+#define fi first
+#define se second
+#define INF 2000000000
+
+using namespace std;
+
+signed main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+    int n, c;
+	cin >> n >> c;
+    vector<int> x(n);
+    vector<int> x1(n), x2(n); 
+    vector<int> y(n);
+    for (int i = 0; i < n; i++) {
+        cin >> x[i];
+    }
+    for (int i = 0; i < n; i++) {
+        x1[i] = x[i] - c*i;
+    }
+    for (int i = 0; i < n; i++) {
+        x2[i] = x[i] + c*i;
+    }
+    int pre_min = x[0];
+    int pre_max = x[0];
+    int min_idx = 0;
+    int max_idx = 0;
+    y[0] = 0;
+    int sumc = 0;
+	for (int i = 1; i < n; i++) {
+        int mx = 0;
+        sumc += c;
+        mx = max(mx, abs(pre_max - x[i]) - sumc + max_idx * c);
+        mx = max(mx, abs(pre_min - x[i]) - sumc + min_idx * c);
+        y[i] = mx;
+        if (pre_max + max_idx * c < x[i] + sumc) {
+            pre_max = x[i];
+            max_idx = i;
+        }
+        if (-pre_min + min_idx * c < -x[i] + sumc) {
+            pre_min = x[i];
+            min_idx = i;
+        }
+        //cout << max_idx << " " << min_idx << "\n";
+	}
+    for (int i = 0; i < n; i++) {
+        cout << y[i] << " ";
+    }
+  
+	return 0;
+}
