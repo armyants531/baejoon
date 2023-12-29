9363317b81f004af75c8c50c9009f7dc9760ab15
CHEOLHWAN BAE <70792311+armyants531@users.noreply.github.com>
2023-12-30 02:02:00 +0900

    [Gold V] Title: Marked-Numbered, Time: 160 ms, Memory: 9836 KB, Score: 3 point -BaekjoonHub

--- 백준/Gold/28270. Marked－Numbered/Marked－Numbered.cc
+++ 백준/Gold/28270. Marked－Numbered/Marked－Numbered.cc
@@ -1,4 +1,3 @@
-// Marked - Numbered  // stack
 #include <bits/stdc++.h>
 
 using namespace std;
@@ -10,55 +9,37 @@ int main() {
 	cout.tie(NULL);
 	int C;
 	cin >> C;
-	int m;
-	stack<pair<int, int>> st; // {m, idx}
-	st.push({ 0, 0 });
-	bool corr = true;
-	int len = 1;
-	vector<int> ans(C + 1);
+	bool correct = true;
+	vector<int> visited(C + 1);
+	int pre = 0;
+	vector<int> m(C + 1);
 	for (int i = 1; i <= C; i++) {
-		cin >> m;
-		if (st.top().first + 1 < m) {
-			corr = false;
+		cin >> m[i];
+		if (i == 1) {
+			if (m[i] != 1) correct = false;  
 		}
-		else if (st.top().first <= m) {
-			st.push({ m, i });
+		else if (m[i - 1] < m[i] - 1) {
+			correct = false;  
 		}
-		else {
-			queue<int> idx_q;
-			while (st.top().first > m) {
-				int idx = st.top().second;
-				st.pop();
-				idx_q.push(idx);
-			}
-			while (!idx_q.empty()) {
-				ans[idx_q.front()] = idx_q.size();
-				idx_q.pop();
-			}
-			st.push({ m, i });
-		}
-	}
-	int end = st.top().first;
-	for (int i = end - 1; i >= 0; i--) {
-		queue<int> idx_q;
-		while (st.top().first > i) {
-			int idx = st.top().second;
-			st.pop();
-			idx_q.push(idx);
-		}
-		while (!idx_q.empty()) {
-			ans[idx_q.front()] = idx_q.size();
-			idx_q.pop();
-		}
-	}
-	if (!corr) {
+	}	
+	if (!correct) {
 		cout << -1 << "\n";
-	}
+	} 
 	else {
 		for (int i = 1; i <= C; i++) {
-			cout << ans[i] << " ";
+			if (m[i - 1] > m[i]) { // if previous num is bigger 
+				visited[m[i]]++;
+				cout << visited[m[i]] << " ";
+			}
+			else if (m[i - 1] < m[i]) { // if previous num is smaller
+				visited[m[i]] = 1;
+				cout << visited[m[i]] << " ";
+			}
+			else { // m[i - 1] == m[i]
+				visited[m[i]]++;
+				cout << visited[m[i]] << " ";
+			}
 		}
 	}
-
 	return 0;
 }
