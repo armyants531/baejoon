c37e52c1f620efb2e423ead041ca049c6cb77e2f
CHEOLHWAN BAE <70792311+armyants531@users.noreply.github.com>
2024-01-24 01:14:54 +0900

    [Silver III] Title: 대한민국을 지키는 가장 긴 힘, Time: 0 ms, Memory: 2024 KB, Score: 24 point -BaekjoonHub

---
+++ 백준/Silver/31263. 대한민국을 지키는 가장 긴 힘/대한민국을 지키는 가장 긴 힘.cc
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+#define int long long
+#define fi first
+#define se second
+
+using namespace std;
+using pii = pair<int, int>;
+
+signed main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int N;
+	cin >> N;
+	string s;
+	cin >> s;
+	int i = 0;
+	int cnt = 0;
+	while (i < N) {
+		if (s[i] == '0') {
+			i--;
+			continue;
+		}
+		if (i + 2 < N) {
+			string st = s.substr(0, 3);
+			if (stoi(st) <= 641) {
+				i+= 3;
+			}
+			else {
+				i+= 2;
+			}
+		}
+		else if (i + 1 < N) {
+			i+= 2;
+		}
+		else {
+			i++;
+		}
+		cnt++;
+	}
+	cout << cnt << "\n";
+	return 0;
+}
