1c8b7526e7e6247653c3d5b7542e6d78846dc978
CHEOLHWAN BAE <70792311+armyants531@users.noreply.github.com>
2024-02-25 22:17:02 +0900

    [Gold V] Title: KSA 문자열, Time: 4 ms, Memory: 2912 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/31433. KSA 문자열/KSA 문자열.cc
+++ 백준/Gold/31433. KSA 문자열/KSA 문자열.cc
@@ -8,18 +8,43 @@ int main() {
 	cout.tie(0);
 	string s;
 	cin >> s;
-	string str = "KSA";
-	int idx = 0;
-	int cnt = 0;
+	string str1 = "KSA";
+	string str2 = "SAK";
+	string str3 = "AKS";
+	int idx1 = 0;
+	int idx2 = 0;
+	int idx3 = 0;
+	int cnt1 = 0;
+	int cnt2 = 0;
+	int cnt3 = 0;
 	int n = s.size();
 	for (int i = 0; i < n; i++) {
-		if (str[idx] == s[i]) {
-			idx++;
-			idx %= 3;
-			cnt++;
+		if (str1[idx1] == s[i]) {
+			idx1++;
+			idx1 %= 3;
+			cnt1++;
+		}
+		if (str2[idx2] == s[i]) {
+			idx2++;
+			idx2 %= 3;
+			cnt2++;
+		}
+		if (str3[idx3] == s[i]) {
+			idx3++;
+			idx3 %= 3;
+			cnt3++;
 		}
 	}
+	if (cnt2 == n) {
+		cnt2 -= 1;
+	}
+	if (cnt3 == n) {
+		cnt3 -= 2;
+	}
+	else if (cnt3 == n - 1) {
+		cnt3 -= 1;
+	}
+	int cnt = max({cnt1, cnt2, cnt3});
 	cout << 2*(n - cnt) << "\n";
-	
 	return 0;
 }
