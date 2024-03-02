19efa659d7e3ac02938e5b44690d8cfd9577934e
CHEOLHWAN BAE <70792311+armyants531@users.noreply.github.com>
2024-03-02 13:19:17 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 36 ms, Memory: 4496 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.cc
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.cc
@@ -1,75 +1,70 @@
-// 최소 스패닝 트리  // Kruskal 알고리즘  
-// https://m.blog.naver.com/ndb796/221230994142 참고
 #include <bits/stdc++.h>
 
 using namespace std;
 
-// edge
+typedef long long ll;
+
 struct edge {
-  int node1;
-  int node2;
-  int weight;
-};
-// parent node info array
-int parent[10001];
+	int node1;
+	int node2;
+	int weight;
+}; 
 
-bool compare(const edge& e1, const edge& e2) {
-  return e1.weight < e2.weight;
+vector<int> parent(10001);
+
+bool cmp(edge A, edge B) {
+	return A.weight < B.weight;
 }
 
-// Union-Find Alg
-// get parent node of x
 int get_parent(int x) {
-  if (parent[x] == x)
-    return x;
-  else 
-    return parent[x] = get_parent(parent[x]);
+	if (parent[x] == x) {
+		return x;
+	}
+	else {
+		return parent[x] = get_parent(parent[x]);
+	}
 }
 
-// merge each parent node
 void union_parent(int A, int B) {
-  A = get_parent(A);
-  B = get_parent(B);
-  if (A < B) 
-    parent[B] = A;
-  else
-    parent[A] = B;
+	A = get_parent(A);
+	B = get_parent(B);
+	if (A < B) {
+		parent[B] = A;
+	}
+	else {
+		parent[A] = B;
+	}
 }
 
-// check if A and B have the same parent node
 bool same_parent(int A, int B) {
-  return get_parent(A) == get_parent(B);
+	return get_parent(A) == get_parent(B);
 }
 
 int main() {
-  ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-  int V, E;
-  cin >> V >> E;
-  vector<edge> graph;
-  int A, B, C;
-  for (int i = 0; i < E; i++) {
-    cin >> A >> B >> C;
-    graph.push_back({A, B, C});
-  }
-  sort(graph.begin(), graph.end(), compare);
-  // Union-Find Alg
-  for (int i = 1; i <= V; i++) {
-    parent[i] = i;
-  }
-  // init the sum of weight
-  int sum = 0;
-  for (int i = 0; i < E; i++) { // for all of the edge 
-    // if not generating a cycle
-    int node1 = graph[i].node1;
-    int node2 = graph[i].node2;
-    if (!same_parent(node1, node2)) {
-      sum += graph[i].weight;
-      union_parent(node1, node2);
-    }
-  }
-  cout << sum << "\n";
-  
-  return 0;
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	int V, E;
+	cin >> V >> E;
+	vector<edge> v;
+	for (int i = 0; i < E; i++) {
+		int A, B, C;
+		cin >> A >> B >> C;
+		v.push_back({A, B, C});
+	}
+	sort(v.begin(), v.end(), cmp);
+	for (int i = 1; i <= V; i++) {
+		parent[i] = i;
+	}
+	int sum = 0;
+	for (int i = 0; i < E; i++) {
+		int n1 = v[i].node1;
+		int n2 = v[i].node2;
+		if (!same_parent(n1, n2)) {
+			sum += v[i].weight;
+			union_parent(n1, n2);
+		}
+	}
+	cout << sum << "\n";
+	return 0;
 }
