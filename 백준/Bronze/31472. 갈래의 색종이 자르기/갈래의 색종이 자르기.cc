8880a2a0a0da74037ab0260665724187169660a0
CHEOLHWAN BAE <70792311+armyants531@users.noreply.github.com>
2024-03-05 01:31:10 +0900

    [Bronze III] Title: 갈래의 색종이 자르기, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Bronze/31472. 갈래의 색종이 자르기/갈래의 색종이 자르기.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	int w;
+	cin >> w;
+	cout << 4 * sqrt(2 * w) << "\n";
+	
+	return 0;
+}
