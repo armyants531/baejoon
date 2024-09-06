dd39e4707340377852fe8feffc293d5a7e7b69ba
CHEOLHWAN BAE <70792311+armyants531@users.noreply.github.com>
2024-09-06 22:52:18 +0900

    [Silver IV] Title: 네모의 꿈, Time: 488 ms, Memory: 44620 KB -BaekjoonHub

---
+++ 백준/Silver/32205. 네모의 꿈/네모의 꿈.cc
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+#define int long long
+#define fi first
+#define se second
+
+using namespace std;
+using pii = pair<int, int>;
+
+signed main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int N;
+	cin >> N;
+	unordered_map<int, bool> mp;
+	bool can = false;
+	for (int i = 0; i < N; i++) {
+		int a, b, c;
+		cin >> a >> b >> c;
+		if (mp[a] || mp[b] || mp[c]) {
+			can = true;
+		}
+		mp[a] = true;
+		mp[b] = true;
+		mp[c] = true;
+	}
+	if (can) cout << 1 << "\n";
+	else cout << 0 << "\n";
+	return 0;
+}
