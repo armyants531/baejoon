c2be577a631df674e7c0340f92429d52e65ca6aa
CHEOLHWAN BAE <70792311+armyants531@users.noreply.github.com>
2025-12-16 09:39:16 +0900

    [Bronze IV] Title: MT 준비, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/34891. MT 준비/MT 준비.cc
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#define int long long
+#define fi first
+#define se second
+
+using namespace std;
+using pii = pair<int, int>;
+
+signed main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	float N, M;
+	cin >> N >> M;
+	cout << ceil(N / M) << "\n";
+	
+	return 0;
+}
